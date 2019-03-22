033c03bc5df6618a36008699927bda0963882e36
Jesse Sanders <jesse.sanders@briebug.com>
2019-03-22 11:01:46 -0600

    added files so project can build in vscode

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@angular/core';
 import { CoreModule } from '../core.module';
+import { of } from "rxjs";
 
 @Injectable({
   providedIn: CoreModule,
 })
 export class AuthService {
+  login() {
+    return of({});
+  }
 }
--- src/app/state/auth/auth.effects.ts
+++ src/app/state/auth/auth.effects.ts
@@ -1,43 +1,37 @@
-import { Injectable } from '@angular/core';
-import { Router } from '@angular/router';
-import { Actions, Effect, ofType } from '@ngrx/effects';
-import { of } from 'rxjs';
-import { catchError, exhaustMap, mergeMap, map, tap } from 'rxjs/operators';
+import { Injectable } from "@angular/core";
+import { Router } from "@angular/router";
+import { Actions, Effect, ofType } from "@ngrx/effects";
+import { of } from "rxjs";
+import { catchError, exhaustMap, mergeMap, map, tap } from "rxjs/operators";
 
-import { AuthService } from '../../core/services/auth.service'
-import { User } from '../../core/models/user';
-import { AppState } from '../app.interfaces';
+import { AuthService } from "../../core/services/auth.service";
+import { User } from "../../core/models/user";
+import { AppState } from "../app.interfaces";
 import {
   AuthActionType,
   Login,
   LoginFailure,
   LoginSuccess
-} from './auth.actions';
+} from "./auth.actions";
 
 @Injectable()
 export class AuthEffects {
-
   @Effect()
-  login = this.actions
-    .pipe(
-      ofType(AuthActionType.Login),
-  mergeMap(() => this.authService.login()
-  .pipe(
-    map((claims: any) => new Login({ claims: claims })),
-    catchError(err => of(new LoginFailure()))
-  ))
-     
-  )
-};
+  login = this.actions.pipe(
+    ofType(AuthActionType.Login),
+    mergeMap(() =>
+      this.authService.login().pipe(
+        map((claims: any) => new LoginSuccess({ claims: claims })),
+        catchError(err => of(new LoginFailure()))
+      )
+    )
+  );
 
-  @Effect({
-    dispatch: false
-  })
-  loginSuccess = this.actions
-  .pipe(
+  @Effect({ dispatch: false })
+  loginSuccess = this.actions.pipe(
     ofType(AuthActionType.LoginSuccess),
     tap(() => {
-      this.router.navigate(['/home']);
+      this.router.navigate(["/home"]);
     })
   );
 
